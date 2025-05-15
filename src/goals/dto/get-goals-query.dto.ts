a7109e08e0271ef5e27d8f60200ac5f221873d02
Walther Oliveira <waltherpires@hotmail.com>
2025-05-15 03:10:27 -0300

    feature: implementa endpoints de progress complete e summary Goal

--- src/goals/dto/get-goals-query.dto.ts
+++ src/goals/dto/get-goals-query.dto.ts
@@ -1,8 +1,20 @@
-import { IsEnum, IsOptional } from "class-validator";
+import { IsArray, IsEnum, IsNumber, IsOptional } from "class-validator";
 import { GoalStatus } from "../goal.entity";
+import { Type } from "class-transformer";
 
 export class GetGoalsQueryDto {
     @IsOptional()
-    @IsEnum(GoalStatus, { message: 'Status inválido!' })
-    status?: GoalStatus;
+    @IsEnum(GoalStatus, { each: true, message: 'Status inválido!' })
+    @IsArray()
+    status?: GoalStatus[];
+
+    @IsOptional()
+    @Type(() => Number)
+    @IsNumber()
+    month?: number;
+
+    @IsOptional()
+    @Type(() => Number)
+    @IsNumber()
+    year?: number;
 }
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }));
   app.use(cookieParser());
   app.enableCors({
     origin: 'http://localhost:3000',
