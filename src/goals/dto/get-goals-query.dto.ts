596c206b9ae53ea8972bff888b21d240decbd147
Walther Oliveira <waltherpires@hotmail.com>
2025-06-06 01:14:31 -0300

    fix: adapts goals fetching for psychologist

--- src/goals/dto/get-goals-query.dto.ts
+++ src/goals/dto/get-goals-query.dto.ts
@@ -1,22 +1,25 @@
-import { IsArray, IsEnum, IsNumber, IsOptional } from "class-validator";
-import { GoalStatus } from "../goal.entity";
-import { Type } from "class-transformer";
+import { IsArray, IsEnum, IsNumber, IsOptional, IsUUID } from 'class-validator';
+import { GoalStatus } from '../goal.entity';
+import { Type } from 'class-transformer';
 
 export class GetGoalsQueryDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  userId?: number;
 
-    
-    @IsOptional()
-    @IsEnum(GoalStatus, { each: true, message: 'Status inválido!' })
-    @IsArray()
-    status?: GoalStatus[];
+  @IsOptional()
+  @IsEnum(GoalStatus, { each: true, message: 'Status inválido!' })
+  @IsArray()
+  status?: GoalStatus[];
 
-    @IsOptional()
-    @Type(() => Number)
-    @IsNumber()
-    month?: number;
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  month?: number;
 
-    @IsOptional()
-    @Type(() => Number)
-    @IsNumber()
-    year?: number;
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  year?: number;
 }
